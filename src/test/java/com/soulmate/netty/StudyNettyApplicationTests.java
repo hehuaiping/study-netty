8a7b6379290f62761d66dd385129b474330fa263
Huaiping He <1424957713@qq.com>
2020-05-25 19:36:43 +0800

    init commit

---
+++ src/test/java/com/soulmate/netty/StudyNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulmate.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
