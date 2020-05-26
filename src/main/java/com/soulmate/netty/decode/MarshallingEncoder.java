6b144ceffefa1e55830555d720f9cac75bb66060
soulmate <1424957713@qq.com>
2020-05-26 17:26:59 +0800

    理解长度占位符的意义 每一个对象序列化之后码流长度是不固定的 故需要在写入对象之前先写入一个符合业务对象长度的占位符 再对象写入之后，在计算对象的码流长度，然后更新长度占位符字段 接收端接收到码流之后根据对象码流字段获取对象序列化码流 然后再解码

--- src/main/java/com/soulmate/netty/decode/MarshallingEncoder.java
+++ src/main/java/com/soulmate/netty/decode/MarshallingEncoder.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  */
 public class MarshallingEncoder {
     /**
-     * 默认长度
+     * 长度占位符
      */
     private static final byte[] LENGTH_PLACEHOLDER = new byte[4];
 
@@ -22,14 +22,20 @@ public class MarshallingEncoder {
     }
 
     protected void encode(Object msg, ByteBuf out) throws IOException {
-        // 已写入长度位置
         try {
+            // 已写入长度位置
             int lengthPos = out.writerIndex();
+            // 先写入32bit占位符（我理解的是用32bit表示这个对象的码流长度，传输到接收端到时候，接收端用这个长度字段读取对象码流，
+            // 然后再解码出对象
             out.writeBytes(LENGTH_PLACEHOLDER);
             ChannelBufferByteOutput output = new ChannelBufferByteOutput(out);
             marshaller.start(output);
             marshaller.writeObject(msg);
+            // 28
             marshaller.finish();
+            // 对象序列化完成后，根据writerIndex 计算对象码流长度，更新占位符数据
+            // setInt方法不会改变writerIndex和readerIndex
+            // 用当前已写入字节长度 - 占位符之前已写入字节长度 - 减去占位符长度 = 对象码流字节长度
             out.setInt(lengthPos, out.writerIndex() - lengthPos - 4);
         }finally {
             marshaller.close();
